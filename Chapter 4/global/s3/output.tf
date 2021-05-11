ba0fd2b7af897ecc61f7124b4b123a36b7508b24
wwdcr2 <wwdcr2@gmail.com>
2021-05-11 11:28:35 +0900

    20210511-1

--- Chapter 4/global/s3/output.tf
+++
@@ -1,6 +0,0 @@
-output "s3_bucket_arn"{
-  value = aws_s3_bucket.terraform_state.arn
-}
-output "dynamo_arn"{
-  value = aws_dynamodb_table.terraform_lock.arn
-}
--- Chapter 4/modules/services/webserver-cluster/user-data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#위에 #! 앞에 공백 있으니 오류나서 적용안됨 꼭 앞에 공백없이 작성 할 것 !!!
-cat > index.html <<EOF
-<h1>Hello, World</h1>
-<p>DB address: ${db_address}</p>
-<p>DB port: ${db_port}</p>
-EOF
-
-nohup busybox httpd -f -p "${server_port}" &yes
